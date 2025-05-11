6f6a28b04fa3da3f6f8a0063452e42ff02adcc2c
Your Name <ayush.0.kumar2224@gmail.com>
2025-05-11 21:13:57 +0530

    Initial commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+--e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Force open Snacks dashboard when starting nvim with no files
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("snacks.dashboard").open()
+    end
+  end,
+})
