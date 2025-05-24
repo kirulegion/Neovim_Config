90ca749f942947e19a70b8245526dbe6c522057d
Your Name <ayush.0.kumar2224@gmail.com>
2025-05-25 02:20:54 +0530

    Updating lsp and making it better.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,110 +1,126 @@
--- Ensure required plugins are installed
--- Install using a plugin manager like lazy.nvim or packer.nvim
-local plugins = {
-	"neovim/nvim-lspconfig",           -- LSP configuration
-	"williamboman/mason.nvim",         -- Manage LSP servers
-	"williamboman/mason-lspconfig.nvim", -- Bridge mason and lspconfig
-	"hrsh7th/cmp-nvim-lsp",            -- LSP autocompletion
-	"hrsh7th/nvim-cmp",                -- Completion engine
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {},
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      -- LSP Configuration
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local on_attach = function(client, bufnr)
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set("n", "<leader>wl", function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+        vim.keymap.set("n", "<leader>f", function()
+          vim.lsp.buf.format({ async = true })
+        end, bufopts)
+      end
+
+      -- Setup language servers
+      lspconfig.tsserver.setup({
+        on_attach = on_attach,
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        capabilities = capabilities,
+      })
+
+      lspconfig.rust_analyzer.setup({
+        on_attach = on_attach,
+        filetypes = { "rust" },
+        capabilities = capabilities,
+      })
+
+      lspconfig.pyright.setup({
+        on_attach = on_attach,
+        filetypes = { "python" },
+        capabilities = capabilities,
+      })
+
+      lspconfig.clangd.setup({
+        on_attach = on_attach,
+        filetypes = { "c", "cpp" },
+        capabilities = capabilities,
+      })
+
+      -- nvim-cmp Configuration
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -- Command-line completion
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" },
+        },
+      })
+
+      -- Search completion
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+    end,
+  },
 }
-
--- Initialize mason and mason-lspconfig
-require("mason").setup()
-require("mason-lspconfig").setup({
-	ensure_installed = { "rust_analyzer", "tsserver", "pyright", "clangd" },
-})
-
--- Setup nvim-cmp for autocompletion
-local cmp = require("cmp")
-cmp.setup({
-	mapping = {
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	},
-	sources = {
-		{ name = "nvim_lsp" },
-	},
-})
-
--- LSP capabilities
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
--- LSP server configurations
-local lspconfig = require("lspconfig")
-
--- Rust (rust-analyzer)
-lspconfig.rust_analyzer.setup({
-	capabilities = capabilities,
-	settings = {
-		["rust-analyzer"] = {
-			diagnostics = {
-				enable = true,
-			},
-			checkOnSave = {
-				command = "clippy",
-			},
-		},
-	},
-})
-
--- TypeScript/TSX/JavaScript/JSX (tsserver)
-lspconfig.tsserver.setup({
-	capabilities = capabilities,
-	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-	settings = {
-		typescript = {
-			inlayHints = {
-				includeInlayParameterNameHints = "all",
-				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayVariableTypeHints = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayEnumMemberValueHints = true,
-			},
-		},
-	},
-})
-
--- Python (pyright)
-lspconfig.pyright.setup({
-	capabilities = capabilities,
-	settings = {
-		python = {
-			analysis = {
-				autoSearchPaths = true,
-				useLibraryCodeForTypes = true,
-				diagnosticMode = "workspace",
-			},
-		},
-	},
-})
-
--- C++ (clangd)
-lspconfig.clangd.setup({
-	capabilities = capabilities,
-	filetypes = { "c", "cpp", "objc", "objcpp" },
-	cmd = { "clangd", "--background-index" },
-})
-
--- Keybindings for LSP
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-	end,
-})
-
--- Enable diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = true,
-	signs = true,
-	update_in_insert = false,
-})
