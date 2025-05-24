f44f96d2d085c8b352eea04f3cebf7768e6eaa11
Your Name <ayush.0.kumar2224@gmail.com>
2025-05-25 01:58:01 +0530

    updating the lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,169 +1,110 @@
-return {
-  -- mason
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "css-lsp",
-        "prettierd",
-      })
-    end,
-  },
-
-  -- typescript.nvim: better tsserver
-  {
-    "jose-elias-alvarez/typescript.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-  },
-
-  -- lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = true },
-      servers = {
-        cssls = {},
-        tailwindcss = {
-          root_dir = function(...)
-            return require("lspconfig.util").root_pattern(".git")(...)
-          end,
-        },
-        tsserver = {
-          root_dir = function(...)
-            return require("lspconfig.util").root_pattern(".git")(...)
-          end,
-          single_file_support = false,
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "literal",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
-        },
-        html = {},
-        lua_ls = {
-          single_file_support = true,
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = {
-                workspaceWord = true,
-                callSnippet = "Both",
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-              diagnostics = {
-                disable = { "incomplete-signature-doc", "trailing-space" },
-                groupSeverity = {
-                  strong = "Warning",
-                  strict = "Warning",
-                },
-                groupFileStatus = {
-                  ["ambiguity"] = "Opened",
-                  ["await"] = "Opened",
-                  ["codestyle"] = "None",
-                  ["duplicate"] = "Opened",
-                  ["global"] = "Opened",
-                  ["luadoc"] = "Opened",
-                  ["redefined"] = "Opened",
-                  ["strict"] = "Opened",
-                  ["strong"] = "Opened",
-                  ["type-check"] = "Opened",
-                  ["unbalanced"] = "Opened",
-                  ["unused"] = "Opened",
-                },
-                unusedLocalExclude = { "_*" },
-              },
-              format = {
-                enable = false,
-                defaultConfig = {
-                  indent_style = "space",
-                  indent_size = "2",
-                  continuation_indent_size = "2",
-                },
-              },
-            },
-          },
-        },
-        eslint = false, -- fully disabled
-      },
-      setup = {
-        eslint = function()
-          return false
-        end,
-        tsserver = function(_, opts)
-          -- Disable formatting in tsserver
-          opts.on_attach = function(client, _)
-            client.server_capabilities.documentFormattingProvider = false
-          end
-          require("typescript").setup({ server = opts })
-          return true -- prevent default setup
-        end,
-      },
-    },
-  },
-
-  -- emoji completion
-  {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-
-  -- null-ls (formatting with prettierd)
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      vim.list_extend(opts.sources, {
-        nls.builtins.formatting.prettierd,
-      })
-    end,
-  },
-
-  -- format on save
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end,
-  },
+-- Ensure required plugins are installed
+-- Install using a plugin manager like lazy.nvim or packer.nvim
+local plugins = {
+	"neovim/nvim-lspconfig",           -- LSP configuration
+	"williamboman/mason.nvim",         -- Manage LSP servers
+	"williamboman/mason-lspconfig.nvim", -- Bridge mason and lspconfig
+	"hrsh7th/cmp-nvim-lsp",            -- LSP autocompletion
+	"hrsh7th/nvim-cmp",                -- Completion engine
 }
+
+-- Initialize mason and mason-lspconfig
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "rust_analyzer", "tsserver", "pyright", "clangd" },
+})
+
+-- Setup nvim-cmp for autocompletion
+local cmp = require("cmp")
+cmp.setup({
+	mapping = {
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+	},
+})
+
+-- LSP capabilities
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- LSP server configurations
+local lspconfig = require("lspconfig")
+
+-- Rust (rust-analyzer)
+lspconfig.rust_analyzer.setup({
+	capabilities = capabilities,
+	settings = {
+		["rust-analyzer"] = {
+			diagnostics = {
+				enable = true,
+			},
+			checkOnSave = {
+				command = "clippy",
+			},
+		},
+	},
+})
+
+-- TypeScript/TSX/JavaScript/JSX (tsserver)
+lspconfig.tsserver.setup({
+	capabilities = capabilities,
+	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	settings = {
+		typescript = {
+			inlayHints = {
+				includeInlayParameterNameHints = "all",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+			},
+		},
+	},
+})
+
+-- Python (pyright)
+lspconfig.pyright.setup({
+	capabilities = capabilities,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+				diagnosticMode = "workspace",
+			},
+		},
+	},
+})
+
+-- C++ (clangd)
+lspconfig.clangd.setup({
+	capabilities = capabilities,
+	filetypes = { "c", "cpp", "objc", "objcpp" },
+	cmd = { "clangd", "--background-index" },
+})
+
+-- Keybindings for LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+	end,
+})
+
+-- Enable diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+})
