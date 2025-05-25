25a647e915296963d3fd2666cc8ffdfcfb50d0e0
Your Name <ayush.0.kumar2224@gmail.com>
2025-05-25 13:05:18 +0530

    updating the lsp and telescope setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,57 +1,43 @@
 return {
   {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "ts_ls",
+          "tailwindcss",
+          "pyright",
+        },
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
     config = function()
       local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local on_attach = function(_, bufnr)
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-        vim.keymap.set("n", "<leader>wl", function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, bufopts)
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-        vim.keymap.set("n", "<leader>f", function()
-          vim.lsp.buf.format({ async = true })
-        end, bufopts)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set({ "n" }, "<space>ca", vim.lsp.buf.code_action, {})
       end
 
-      lspconfig.tsserver.setup({
-        on_attach = on_attach,
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        capabilities = capabilities,
-      })
+      lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, { on_attach = on_attach })
 
-      lspconfig.rust_analyzer.setup({
-        on_attach = on_attach,
-        filetypes = { "rust" },
-        capabilities = capabilities,
-      })
-
-      lspconfig.pyright.setup({
-        on_attach = on_attach,
-        filetypes = { "python" },
-        capabilities = capabilities,
-      })
-
-      lspconfig.clangd.setup({
-        on_attach = on_attach,
-        filetypes = { "c", "cpp" },
-        capabilities = capabilities,
-      })
+      lspconfig.lua_ls.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.tailwindcss.setup({})
     end,
   },
 }
