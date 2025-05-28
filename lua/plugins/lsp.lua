5e3b615fff61d3c91cfb16a2e53dc9f1a5aab59c
Your Name <ayush.0.kumar2224@gmail.com>
2025-05-28 12:29:21 +0530

    lsp changes for preformance

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,12 @@
 return {
   {
-    "williamboman/mason.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    "mason-org/mason.nvim",
     config = function()
       require("mason").setup()
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "williamboman/mason.nvim" },
+    "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -17,74 +14,30 @@ return {
           "rust_analyzer",
           "ts_ls",
           "tailwindcss",
-          "html",
         },
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    ft = { "lua", "rust", "typescript", "javascript", "python", "css", "c", "cpp", "json", "html", "zig", "odin" },
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-    },
     config = function()
       local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Shared on_attach function
       local on_attach = function(_, bufnr)
-        local map = function(keys, func, desc)
-          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
-        end
-
-        -- Core keymaps from LSP-1
-        map("K", vim.lsp.buf.hover, "Hover Documentation")
-        map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-        map("gd", vim.lsp.buf.definition, "Goto Definition")
-        map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-
-        -- Additional keymaps from LSP-2, optimized
-        local tele = require("telescope.builtin")
-        map("<leader>fr", tele.lsp_references, "Goto References")
-        map("<leader>rn", vim.lsp.buf.rename, "Rename")
-        map("<leader>E", vim.diagnostic.open_float, "Show Diagnostics")
-        map("<leader>wf", vim.lsp.buf.format, "Format Buffer")
-
-        -- Visual mode code action
-        vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "LSP: Code Action" })
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set({ "n" }, "<space>ca", vim.lsp.buf.code_action, {})
       end
 
-      -- Configure each server with capabilities and on_attach
-      local servers = {
-        "lua_ls",
-        "rust_analyzer",
-        "ts_ls",
-        "tailwindcss",
-        "pyright",
-        "clangd",
-        "jsonls",
-        "html",
-        "zls",
-        "ols",
-      }
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        })
-      end
+      lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, { on_attach = on_attach })
 
-      -- Optional: Add jdtls or eslint for specific projects
-      -- Example for jdtls (uncomment and configure as needed)
-      -- lspconfig.jdtls.setup({
-      --   capabilities = capabilities,
-      --   on_attach = on_attach,
-      --   root_dir = lspconfig.util.root_pattern("pom.xml", "build.gradle", ".git"),
-      -- })
+      lspconfig.lua_ls.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.tailwindcss.setup({})
     end,
   },
 }
