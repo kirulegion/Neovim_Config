9e89289efd1c9036140441916ccad52883b451d4
Your Name <ayush.0.kumar2224@gmail.com>
2025-05-29 11:01:49 +0530

    making telescope lighter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,48 +1,25 @@
 return {
-		{
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x", -- Use a stable branch
-    lazy = false, -- Prevent lazy loading
-    dependencies = { "nvim-lua/plenary.nvim" }, -- Required dependency
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      -- Minimal setup with no customizations
-      local ok, telescope = pcall(require, "telescope")
-      if not ok then
-        print("Failed to load telescope.nvim")
-        return
-      end
-      telescope.setup({})
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+
+      require("telescope").load_extension("ui-select")
     end,
   },
-		{
-  "nvim-telescope/telescope-ui-select.nvim",
-  config = function()
-				-- This is your opts table
-require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-        -- even more opts
-      }
-
-      -- pseudo code / specification for writing custom displays, like the one
-      -- for "codeactions"
-      -- specific_opts = {
-      --   [kind] = {
-      --     make_indexed = function(items) -> indexed_items, width,
-      --     make_displayer = function(widths) -> displayer
-      --     make_display = function(displayer) -> function(e)
-      --     make_ordinal = function(e) -> string
-      --   },
-      --   -- for example to disable the custom builtin "codeactions" display
-      --      do the following
-      --   codeactions = false,
-      -- }
-    }
-  }
 }
--- To get ui-select loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require("telescope").load_extension("ui-select")
-  end,
-}}
