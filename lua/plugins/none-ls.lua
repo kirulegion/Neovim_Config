2d68b5a4b973864517dcdb4c30540cf4a8497642
Your Name <ayush.0.kumar2224@gmail.com>
2025-06-01 21:31:33 +0530

    adding a config for rustfmt

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,12 +1,26 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-      },
-    })
-  end,
+  { "https://github.com/rust-lang/rustfmt" },
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      local rustfmt = {
+        name = "rustfmt",
+        method = null_ls.methods.FORMATTING,
+        filetypes = { "rust" },
+        generator = null_ls.formatter({
+          command = "rustfmt",
+          args = { "--emit=stdout" },
+          to_stdin = true,
+        }),
+      }
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+          rustfmt,
+        },
+      })
+    end,
+  },
 }
